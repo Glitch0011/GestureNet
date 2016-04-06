e535e3ae11dbbe85923f6f70b54fbc1b44b8e408
Tom Bonner <tom@bonner.is>
2016-04-06 19:33:18 +0100

    Added KeyState Control

--- GestureNet.WPFExample/MouseUtilities.cs
+++ GestureNet.WPFExample/MouseUtilities.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Input;
 
 namespace GestureNet.WPFExample
 {
@@ -10,12 +11,34 @@ namespace GestureNet.WPFExample
         [return: MarshalAs(UnmanagedType.Bool)]
         private static extern bool GetCursorPos(ref Win32Point pt);
 
+        [DllImport("user32.dll")]
+        private static extern short GetKeyState(int pt);
+
         [StructLayout(LayoutKind.Sequential)]
         private struct Win32Point
         {
             public Int32 X;
             public Int32 Y;
         };
+        
+        private static int ConvertMouse(MouseButton button)
+        {
+            switch (button)
+            {
+                case MouseButton.Left:
+                    return 0x01;
+                case MouseButton.Right:
+                    return 0x02;
+                case MouseButton.Middle:
+                    return 0x04;
+                case MouseButton.XButton1:
+                case MouseButton.XButton2:
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        public static bool IsMouseButtonDown(MouseButton button) => GetKeyState(ConvertMouse(button)) < 0;
 
         public static Point GetMousePosition()
         {
